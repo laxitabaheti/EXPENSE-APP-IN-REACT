1f96a9b3bd42a0af6968773d878f7bf97c89daab
Laxita <86618594+laxitabaheti@users.noreply.github.com>
2022-01-16 01:05:11 +0530

    update

--- src/NewExpenseForm.js
+++ src/NewExpenseForm.js
@@ -52,7 +52,7 @@ const NewExpenseForm = (props) => {
 
     const expenseData = {
       title: enteredTitle,
-      amount: enteredAmount,
+      amount: +enteredAmount,
       date: new Date(enteredDate)
     };
 
